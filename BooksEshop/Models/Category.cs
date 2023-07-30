f479dec0ff2c42f5e53195737e25270165600886
Tianqi <ttq670801508@gmail.com>
2023-07-31 03:59:57 +1200

    Created Categories pages; Finised server and user sides Validations, Category CRUD operations, Set some UI, Created Toastr notification

--- BooksEshop/Models/Category.cs
+++ BooksEshop/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BooksEshop.Models
 {
@@ -7,7 +8,11 @@ namespace BooksEshop.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [MaxLength(20)]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,1000,ErrorMessage ="Display order should be between 1-1000.")]
         public int DisplayOrder { get; set; }
     }
 }
